957d0be2c98db45752b23925385e80cc49e1051b
Diego Ricardo <diegoricardoweb@gmail.com>
2023-09-07 15:32:39 -0300

    feat: hashed user password

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { ConflictException, Injectable } from '@nestjs/common';
+import { hash } from 'bcryptjs';
 import { PrismaService } from 'src/database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -17,8 +18,34 @@ export class UsersService {
       throw new ConflictException('This email is already in use.');
     }
 
+    const hashedPassword = await hash(password, 12);
+
     const user = await this.prismaService.user.create({
-      data: { name, email, password },
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
     });
 
     return user;
