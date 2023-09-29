809aac35fc671c14c0a5ede3f42b940f8fa87e84
Diego Ricardo <diegoricardoweb@gmail.com>
2023-09-28 23:54:24 -0300

    feat: insert authContext, toast and react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import { Router } from "./Router";
+import { AuthProvider } from "./app/contexts/AuthContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 export function App() {
   return (
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
