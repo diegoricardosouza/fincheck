3bf6a32b659146b7979744f989fd2282719e2739
Diego Ricardo <diegoricardoweb@gmail.com>
2023-09-28 23:52:59 -0300

    feat: create auth context

--- frontend/src/Router/AuthGuard.tsx
+++ frontend/src/Router/AuthGuard.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../app/hooks/useAuth";
 
 interface AuthGuardsProps {
   isPrivate: boolean
 }
 
 export function AuthGuard({ isPrivate }: AuthGuardsProps) {
-  const signedIn = false;
+  const { signedIn } = useAuth();
 
   if (!signedIn && isPrivate) {
     return <Navigate to="/login" replace />
