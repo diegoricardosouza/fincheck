063443910692f2ba7889b0446c84081937d246e6
Diego Ricardo <diegoricardoweb@gmail.com>
2023-10-03 21:03:16 -0300

    feat: create component dropdown transaction

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -175,6 +175,43 @@ export default {
           800: '#E8590C',
           900: '#D9480F',
         },
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideLeftAndFade: {
+          from: { opacity: 0, transform: 'translateX(2px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideUpAndFade: {
+          from: { opacity: 0, transform: 'translateY(2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideRightAndFade: {
+          from: { opacity: 0, transform: 'translateX(-2px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+        },
+      },
+      animation: {
+        'slide-down-and-fade': 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'slide-left-and-fade': 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'slide-up-and-fade': 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'slide-right-and-fade': 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'overlay-show': 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        'content-show': 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
+      fontFamily: {
+        'dm-sans': ['DM Sans', 'sans-serif']
       }
     },
   },
