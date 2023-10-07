24fac15d907b15425777cc9258d337c6fb7a19f8
Diego Ricardo <diegoricardoweb@gmail.com>
2023-10-06 21:07:34 -0300

    feat: create filtersModal component

--- frontend/src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ frontend/src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import { useDashboard } from "../DashboardContext/useDashboard";
 
 export function useTransactionsController() {
   const { areValuesVisible } = useDashboard();
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
+
+  function handleOpenFilterModal() {
+    setIsFilterModalOpen(true);
+  }
+
+  function handleCloseFilterModal() {
+    setIsFilterModalOpen(false);
+  }
 
   return {
     areValuesVisible,
     transactions: [],
     isInitialLoading: false,
     isLoading: false,
+    isFilterModalOpen,
+    handleOpenFilterModal,
+    handleCloseFilterModal
   }
 }
