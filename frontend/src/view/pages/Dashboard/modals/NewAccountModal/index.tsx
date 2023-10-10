7d2c911faeef92db8095cdbe84e3074313ce3949
Diego Ricardo <diegoricardoweb@gmail.com>
2023-10-10 00:07:30 -0300

    feat: add create account on the modal

--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,3 +1,4 @@
+import { Controller } from "react-hook-form";
 import { Button } from "../../../../components/Button";
 import { ColorsDropdownInput } from "../../../../components/ColorsDropdownInput";
 import { Input } from "../../../../components/Input";
@@ -8,8 +9,13 @@ import { useNewAccountModalController } from "./useNewAccountModalController";
 
 export function NewAccountModal() {
   const {
+    closeNewAccountModal,
     isNewAccountModalOpen,
-    closeNewAccountModal
+    errors,
+    handleSubmit,
+    register,
+    control,
+    isLoading,
   } = useNewAccountModalController();
 
   return (
@@ -18,52 +24,81 @@ export function NewAccountModal() {
       open={isNewAccountModalOpen}
       onClose={closeNewAccountModal}
     >
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
-          <span className="text-gray-600 tracking-[-0.5px] text-xs">
-            Saldo
-          </span>
+          <span className="text-gray-600 tracking-[-0.5px] text-xs">Saldo inicial</span>
+          <div className="flex items-center gap-2">
+            <span className="text-gray-600 tracking-[-0.5px] text-lg">R$</span>
 
-          <div className="flex items-center justify-center gap-2">
-            <span className="text-gray-600 tracking-[-0.5px] text-lg">
-              R$
-            </span>
-            <InputCurrency />
+            <Controller
+              control={control}
+              name="initialBalance"
+              defaultValue="0"
+              render={({ field: { onChange, value } }) => (
+                <InputCurrency
+                  error={errors.initialBalance?.message}
+                  onChange={onChange}
+                  value={value}
+                />
+              )}
+            />
           </div>
         </div>
 
         <div className="mt-10 flex flex-col gap-4">
           <Input
             type="text"
-            name="name"
             placeholder="Nome da Conta"
+            error={errors.name?.message}
+            {...register('name')}
           />
 
-          <Select
-            placeholder="Tipo"
-            options={[
-              {
-                value: 'CHECKING',
-                label: 'Conta Corrente',
-              },
-              {
-                value: 'INVESTMENT',
-                label: 'Investimentos',
-              },
-              {
-                value: 'CASH',
-                label: 'Dinheiro',
-              }
-            ]}
+          <Controller
+            control={control}
+            name="type"
+            defaultValue="CHECKING"
+            render={({ field: { onChange, value } }) => (
+              <Select
+                placeholder="Tipo"
+                error={errors.type?.message}
+                onChange={onChange}
+                value={value}
+                options={[
+                  {
+                    value: 'CHECKING',
+                    label: 'Conta Corrente',
+                  },
+                  {
+                    value: 'INVESTMENT',
+                    label: 'Investimentos',
+                  },
+                  {
+                    value: 'CASH',
+                    label: 'Dinheiro Físico',
+                  },
+                ]}
+              />
+            )}
           />
 
-          <ColorsDropdownInput />
+          <Controller
+            control={control}
+            name="color"
+            defaultValue=""
+            render={({ field: { onChange, value } }) => (
+              <ColorsDropdownInput
+                error={errors.color?.message}
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
         </div>
 
-        <Button className="w-full mt-6">
+        <Button type="submit" className="w-full mt-6" isLoading={isLoading}>
           Criar
         </Button>
       </form>
     </Modal>
-  )
+  );
 }
