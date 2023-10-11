0a817cf287806f5be72887f8be5f123c497af4c4
Diego Ricardo <diegoricardoweb@gmail.com>
2023-10-10 21:19:59 -0300

    fix: bug fix InputCurrency

--- frontend/src/view/components/InputCurrency.tsx
+++ frontend/src/view/components/InputCurrency.tsx
@@ -1,12 +1,33 @@
-import { NumericFormat } from "react-number-format";
+import { CrossCircledIcon } from '@radix-ui/react-icons';
+import { NumericFormat } from 'react-number-format';
+import { cn } from '../../app/utils/cn';
 
-export function InputCurrency() {
+interface InputCurrencyProps {
+  error?: string;
+  value?: string | number;
+  onChange?(value: string): void;
+}
+
+export function InputCurrency({ error, value, onChange }: InputCurrencyProps) {
   return (
-    <NumericFormat
-      className="w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none"
-      thousandSeparator="."
-      decimalSeparator=","
-      defaultValue={0}
-    />
+    <div>
+      <NumericFormat
+        thousandSeparator="."
+        decimalSeparator=","
+        value={value}
+        onValueChange={event => onChange?.(event.value)}
+        className={cn(
+          'text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none w-full',
+          error && 'text-red-900',
+        )}
+      />
+
+      {error && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-xs">{error}</span>
+        </div>
+      )}
+    </div>
   )
 }
