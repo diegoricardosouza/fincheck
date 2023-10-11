9c1540860e06338bab36949e4c69bb85d000b8b0
Diego Ricardo <diegoricardoweb@gmail.com>
2023-10-10 21:30:55 -0300

    feat: finished newTransactionModal

--- frontend/src/view/pages/Dashboard/modals/NewTransactionModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewTransactionModal/index.tsx
@@ -1,3 +1,4 @@
+import { Controller } from "react-hook-form";
 import { Button } from "../../../../components/Button";
 import { DatePickerInput } from "../../../../components/DatePickerInput";
 import { Input } from "../../../../components/Input";
@@ -10,7 +11,14 @@ export function NewTransactionModal() {
   const {
     closeNewTransactionModal,
     isNewTransactionModalOpen,
-    newTransactionType
+    newTransactionType,
+    control,
+    errors,
+    handleSubmit,
+    register,
+    accounts,
+    categories,
+    isLoading
   } = useNewTransactionModalController();
 
   const isExpense = newTransactionType === 'EXPENSE'
@@ -21,7 +29,7 @@ export function NewTransactionModal() {
       open={isNewTransactionModalOpen}
       onClose={closeNewTransactionModal}
     >
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
           <span className="text-gray-600 tracking-[-0.5px] text-xs">
             Valor {isExpense ? 'da despesa' : 'da receita'}
@@ -31,57 +39,81 @@ export function NewTransactionModal() {
             <span className="text-gray-600 tracking-[-0.5px] text-lg">
               R$
             </span>
-            <InputCurrency />
+
+            <Controller
+              control={control}
+              name="value"
+              defaultValue="0"
+              render={({ field: { onChange, value } }) => (
+                <InputCurrency
+                  error={errors.value?.message}
+                  onChange={onChange}
+                  value={value}
+                />
+              )}
+            />
           </div>
         </div>
 
         <div className="mt-10 flex flex-col gap-4">
           <Input
             type="text"
-            name="name"
             placeholder={isExpense ? 'Nome da Despesa' : 'Nome da Receita'}
+            error={errors.name?.message}
+            {...register('name')}
           />
 
-          <Select
-            placeholder="Categoria"
-            options={[
-              {
-                value: 'CHECKING',
-                label: 'Conta Corrente',
-              },
-              {
-                value: 'INVESTMENT',
-                label: 'Investimentos',
-              },
-              {
-                value: 'CASH',
-                label: 'Dinheiro',
-              }
-            ]}
+          <Controller
+            control={control}
+            name="categoryId"
+            defaultValue=""
+            render={({ field: { onChange, value } }) => (
+              <Select
+                placeholder="Categoria"
+                onChange={onChange}
+                value={value}
+                error={errors.categoryId?.message}
+                options={categories.map(category => ({
+                  value: category.id,
+                  label: category.name
+                }))}
+              />
+            )}
           />
 
-          <Select
-            placeholder={isExpense ? 'Pagar com' : 'Receber com'}
-            options={[
-              {
-                value: 'CHECKING',
-                label: 'Conta Corrente',
-              },
-              {
-                value: 'INVESTMENT',
-                label: 'Investimentos',
-              },
-              {
-                value: 'CASH',
-                label: 'Dinheiro',
-              }
-            ]}
+          <Controller
+            control={control}
+            name="bankAccountId"
+            defaultValue=""
+            render={({ field: { onChange, value } }) => (
+              <Select
+                placeholder={isExpense ? 'Pagar com' : 'Receber com'}
+                onChange={onChange}
+                value={value}
+                error={errors.bankAccountId?.message}
+                options={accounts.map(account => ({
+                  value: account.id,
+                  label: account.name
+                }))}
+              />
+            )}
           />
 
-          <DatePickerInput />
+          <Controller
+            control={control}
+            name="date"
+            defaultValue={new Date()}
+            render={({ field: { onChange, value } }) => (
+              <DatePickerInput
+                error={errors.date?.message}
+                value={value}
+                onChange={onChange}
+              />
+            )}
+          />
         </div>
 
-        <Button className="w-full mt-6">
+        <Button className="w-full mt-6" isLoading={isLoading}>
           Criar
         </Button>
       </form>
--- frontend/src/view/pages/Dashboard/modals/NewTransactionModal/useNewTransactionModalController.ts
+++ frontend/src/view/pages/Dashboard/modals/NewTransactionModal/useNewTransactionModalController.ts
@@ -1,4 +1,23 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+import { useBankAccounts } from "../../../../../app/hooks/useBankAccounts";
+import { useCategories } from "../../../../../app/hooks/useCategories";
+import { transactionsService } from "../../../../../app/services/transactionsService";
 import { useDashboard } from "../../components/DashboardContext/useDashboard";
+
+const schema = z.object({
+  value: z.string().nonempty('Informe o valor'),
+  name: z.string().nonempty('Informe o nome'),
+  categoryId: z.string().nonempty('Informe a categoria'),
+  bankAccountId: z.string().nonempty('Informe a conta'),
+  date: z.date(),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function useNewTransactionModalController() {
   const {
@@ -7,9 +26,64 @@ export function useNewTransactionModalController() {
     newTransactionType
   } = useDashboard();
 
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+    control,
+    reset
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  });
+
+  const queryClient = useQueryClient();
+  const { accounts } = useBankAccounts();
+  const { categories: categoriesList } = useCategories();
+  const {
+    isLoading,
+    mutateAsync
+  } = useMutation(transactionsService.create)
+
+  const handleSubmit = hookFormSubmit(async data => {
+    try {
+      await mutateAsync({
+        ...data,
+        value: Number(data.value),
+        type: newTransactionType!,
+        date: data.date.toISOString()
+      });
+
+      queryClient.invalidateQueries({ queryKey: ['transactions'] });
+      toast.success(
+        newTransactionType === 'EXPENSE'
+          ? 'Despesa cadastrada com sucesso!'
+          : 'Receita cadastrada com sucesso!'
+      );
+      closeNewTransactionModal();
+      reset();
+    } catch {
+      toast.error(
+        newTransactionType === 'EXPENSE'
+          ? 'Erro ao cadastrar a despesa!'
+          : 'Erro ao cadastrar a receita!'
+      );
+    }
+  });
+
+  const categories = useMemo(() => {
+    return categoriesList.filter(category => category.type === newTransactionType);
+  }, [categoriesList, newTransactionType]);
+
   return {
     isNewTransactionModalOpen,
     closeNewTransactionModal,
-    newTransactionType
+    newTransactionType,
+    register,
+    errors,
+    control,
+    handleSubmit,
+    accounts,
+    categories,
+    isLoading
   }
 }
