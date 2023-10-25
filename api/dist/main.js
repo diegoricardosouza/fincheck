8946ec7330b73ff033c0792060920813c3c5ce55
Diego Ricardo <diegoricardoweb@gmail.com>
2023-10-25 00:09:51 -0300

    fix

--- api/dist/main.js
+++ api/dist/main.js
@@ -7,7 +7,9 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
-    app.enableCors();
+    app.enableCors({
+        origin: '*',
+    });
     await app.listen(3000);
 }
 bootstrap();
