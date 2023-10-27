0512911f5604973386c4df3b5d4552fc064fdae7
Diego Ricardo <diegoricardoweb@gmail.com>
2023-10-27 20:09:33 -0300

    feat: add pwa config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,38 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'vite-react-ts-100',
+        short_name: 'vite-react-ts-100',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#087F5B',
+        lang: 'pt-br',
+        scope: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#087F5B',
+      },
+    }),
+  ],
   server: {
     open: true,
   }
